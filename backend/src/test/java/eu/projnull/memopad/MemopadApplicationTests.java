c71e506f9e37e09d7d159f853205b0aac0eff58f
HyScript7 <hyscript7@seznam.cz>
2025-03-04 14:38:04 +0100

    Add backend project

---
+++ backend/src/test/java/eu/projnull/memopad/MemopadApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.projnull.memopad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemopadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
