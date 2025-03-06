048e930f79798546d77663f54cf5014e71bc2e5a
HyScript7 <hyscript7@seznam.cz>
2025-03-06 10:08:00 +0100

    Fix embedded SPA (now supports any path)

--- backend/src/main/java/eu/projnull/memopad/controllers/SinglePageApplicationController.java
+++ backend/src/main/java/eu/projnull/memopad/controllers/SinglePageApplicationController.java
@@ -2,11 +2,15 @@ package eu.projnull.memopad.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
 public class SinglePageApplicationController {
+    @GetMapping("/{path:^(?!static|api)(?!.*\\.[a-zA-Z0-9]+$).+}/**")
+    public String anyPath() {
+        return "forward:/";
+    }
+
     @GetMapping("/")
     public String index() {
         return "forward:/static/index.html";
