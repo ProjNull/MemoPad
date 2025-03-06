75ae17de79b7983b224b970bd24fe2a7c6caf9a6
HyScript7 <hyscript7@seznam.cz>
2025-03-06 10:22:30 +0100

    Add default datasource configuration

---
+++ backend/src/main/java/eu/projnull/memopad/config/DatasourceConfig.java
@@ -0,0 +1,20 @@
+package eu.projnull.memopad.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DatasourceConfig {
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.postgresql.Driver");
+        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/memopad");
+        dataSourceBuilder.username("root");
+        dataSourceBuilder.password("root");
+        return dataSourceBuilder.build();
+    }
+}
