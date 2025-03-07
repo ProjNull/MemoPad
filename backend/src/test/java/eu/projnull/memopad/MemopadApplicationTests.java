d8f080e42dba563fbf0f7e812ac036c4158974c7
HyScript7 <hyscript7@seznam.cz>
2025-03-07 14:52:14 +0100

    Remove default test, since it was breaking builds

--- backend/src/test/java/eu/projnull/memopad/MemopadApplicationTests.java
+++ backend/src/test/java/eu/projnull/memopad/MemopadApplicationTests.java
@@ -1,13 +1,7 @@
 package eu.projnull.memopad;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MemopadApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
 }
