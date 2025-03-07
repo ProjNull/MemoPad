6aa80c608b7025b6cbd0caab9bddf4c4991e2472
HyScript7 <hyscript7@seznam.cz>
2025-03-07 15:50:21 +0100

    Configure datasource via environment variables (Spring Data)

--- backend/src/main/java/eu/projnull/memopad/config/DatasourceConfig.java
+++ backend/src/main/java/eu/projnull/memopad/config/DatasourceConfig.java
@@ -11,10 +11,26 @@ public class DatasourceConfig {
     @Bean
     public DataSource getDataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        String dbHost = System.getenv("PG_HOST");
+        String dbPort = System.getenv("PG_PORT");
+        String dbUser = System.getenv("PG_USER");
+        String dbPassword = System.getenv("PG_PASSWORD");
+        String dbName = System.getenv("PG_DATABASE");
+
+        if (dbHost == null || dbPort == null || dbName == null || dbUser == null || dbPassword == null) {
+            throw new NullPointerException(
+                    "One of the environment variables DB_HOST, DB_PORT, DB_NAME, DB_USER or DB_PASSWORD is not set!");
+        }
+
         dataSourceBuilder.driverClassName("org.postgresql.Driver");
-        dataSourceBuilder.url("jdbc:postgresql://localhost:5432/memopad");
-        dataSourceBuilder.username("root");
-        dataSourceBuilder.password("root");
+        dataSourceBuilder.url("jdbc:postgresql://"
+                + dbHost
+                + ":"
+                + dbPort
+                + "/"
+                + dbName);
+        dataSourceBuilder.username(dbUser);
+        dataSourceBuilder.password(dbPassword);
         return dataSourceBuilder.build();
     }
 }
