656178850dfdb706ab4662b9ef31949372262b45
HyScript7 <hyscript7@seznam.cz>
2025-03-08 19:13:42 +0100

    Implement Note service.

--- backend/src/main/java/eu/projnull/memopad/repositories/NoteRepository.java
+++ backend/src/main/java/eu/projnull/memopad/repositories/NoteRepository.java
@@ -1,6 +1,7 @@
 package eu.projnull.memopad.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.repository.Repository;
 
@@ -8,7 +9,8 @@ import eu.projnull.memopad.models.Folder;
 import eu.projnull.memopad.models.Note;
 
 public interface NoteRepository extends Repository<Note, Long> {
-    void save(Note note);
+    Note save(Note note);
     void delete(Note note);
+    Optional<Note> findById(long id);
     List<Note> findByFolder(Folder folder);
 }
---
+++ backend/src/main/java/eu/projnull/memopad/services/NoteService.java
@@ -0,0 +1,54 @@
+package eu.projnull.memopad.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Service;
+
+import eu.projnull.memopad.models.Folder;
+import eu.projnull.memopad.models.Note;
+import eu.projnull.memopad.repositories.NoteRepository;
+
+@Service
+public class NoteService {
+    private final NoteRepository noteRepository;
+    public NoteService(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+    
+    public Note createNote(long ownerId, String name, String content, Folder folder) {
+        Note note = new Note();
+        note.setName(name);
+        note.setContent(content);
+        note.setFolder(folder);
+        return noteRepository.save(note);
+    }
+
+    public void deleteNote(long ownerId, Note note) {
+        noteRepository.delete(note);
+    }
+
+    public Note renameNote(long ownerId, Note note, String newName) {
+        note.setName(newName);
+        return noteRepository.save(note);
+    }
+
+    public Note moveNote(long ownerId, Note note, Folder newFolder) {
+        note.setFolder(newFolder);
+        return noteRepository.save(note);
+    }
+
+    public @Nullable Note getNote(long ownerId, long noteId) {
+        return noteRepository.findById(noteId).orElse(null);
+    }
+
+    public Note updateNoteContent(long ownerId, Note note, String content) {
+        note.setContent(content);
+        return noteRepository.save(note);
+    }
+
+    public List<Note> getNotesInFolder(long ownerId, Folder folder) {
+        return noteRepository.findByFolder(folder);
+    }
+}
