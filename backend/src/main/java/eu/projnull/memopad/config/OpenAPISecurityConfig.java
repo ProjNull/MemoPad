fde7a122131681db5e353ec2c86f90597e8c4e64
HyScript7 <hyscript7@seznam.cz>
2025-09-25 16:48:07 +0200

    Configure OpenAPI and SwaggerUI

---
+++ backend/src/main/java/eu/projnull/memopad/config/OpenAPISecurityConfig.java
@@ -0,0 +1,21 @@
+package eu.projnull.memopad.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Memopad API", version = "1.2.0", description = "The Spring Boot backend for {NULL} MemoPad"),
+        security = @SecurityRequirement(name = "bearerAuth") // Apply globally
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer", // the actual scheme
+        bearerFormat = "JWT" // Shows JWT in the UI
+)
+public class OpenAPISecurityConfig {}
--- backend/src/main/java/eu/projnull/memopad/config/SecurityConfig.java
+++ backend/src/main/java/eu/projnull/memopad/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/register", "/api/auth/login", "/static/**", "/{name:^(?!api).+}/**", "/").permitAll()
+                        .requestMatchers("/api/auth/register", "/api/auth/login", "/static/**", "/{name:^(?!api).+}/**", "/", "/api/docs/**", "/api/api-docs/**", "/api/swagger-ui/**").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider())
