8584077403bbb533cfefc2fd361ea1cdb94521e2
Jan Palma <honzik@janpalma.cz>
2026-01-08 08:54:17 +0100

    Added better Proxy Settings

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,34 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+
+
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  const base = {
+    plugins: [
+      vue(),
+      vueDevTools(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      },
     },
-  },
-  server: {
-    proxy: {
-      '/api': { target: "http://localhost:8800/", changeOrigin: true, }
-    }
+    server: {}
   }
+
+  if (env.VITE_API_PROXY) {
+    console.log("\n=== Using API Proxy! ===\n")
+    base.server["proxy"] = {
+      '/api': { target: env.VITE_API_PROXY , changeOrigin: true, }
+    } 
+  }
+
+  return base;
 })
