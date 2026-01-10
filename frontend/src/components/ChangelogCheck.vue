ea69326d40232bd4c8a00d895fd9b8b0b3a8942c
Jan Palma <honzik@janpalma.cz>
2026-01-10 11:10:48 +0100

    Changelog fix

--- frontend/src/components/ChangelogCheck.vue
+++ frontend/src/components/ChangelogCheck.vue
@@ -3,6 +3,7 @@ import { onMounted, ref, useTemplateRef } from 'vue';
 
 import changelogAPI, { type Changelog } from '@/api/cls.ts';
 import Modal from './Modal.vue';
+import { marked } from 'marked';
 
 const modal = useTemplateRef("modal");
 const content = ref("");
@@ -10,28 +11,35 @@ const title = ref("");
 
 onMounted(() => {
     
-    changelogAPI.isLatest().then((d) => {
+    changelogAPI.isLatest().then(async (d) => {
         if (d) {
-            modal.value?.open();
             console.log("test");
-            content.value = d.description
+            content.value = await marked.parse(d.description)
             title.value = d.title
+            modal.value?.open();
         }
 
     })
 })
+
+function close() {
+    if (modal.value) modal.value.close();
+}
 </script>
 
 <template>
-    <Modal ref="modal" class="min-h-90 max-h-screen">
+    <Modal ref="modal" class="min-h-90 max-h-screen flex flex-col md:max-h-2/3">
         <div class="flex items-center pb-2 mb-2 border-b border-b-base-200">
             <h1 class="text-2xl font-bold grow">Changelog</h1>
-            <button class="btn btn-square" @click="modal?.close()"><i class="bi bi-x-lg"></i></button>
+            <button class="btn btn-square" @click="close()"><i class="bi bi-x-lg"></i></button>
         </div>
         
-        <h2 class="text-lg font-bold">{{ title }}</h2>
-        <div>
-            {{ content }}
+        <div class="grow overflow-y-auto">
+            <h2 class="text-lg font-bold">{{ title }}</h2>
+            <div class="divider my-2"></div>
+            <div class="prose prose-invert prose-sm" :innerHTML="content">
+            </div>
+
         </div>
     </Modal>
 </template>
